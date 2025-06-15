9fb961b64f5e089ea65b5cd915dafb70d30320d1
ShrBox <53301243+ShrBox@users.noreply.github.com>
2025-06-15 22:34:09 +0800

    chore: replace buildir with builddir to be compatible with xmake 3.0.0

--- rules/linkrule.lua
+++ rules/linkrule.lua
@@ -4,7 +4,7 @@ on_config(function(target)
 end)
 before_link(function(target)
     import("lib.detect.find_file")
-    os.mkdir("$(buildir)/.prelink/lib")
+    os.mkdir("$(builddir)/.prelink/lib")
 
     local data = assert(find_file("bedrock_runtime_data", {"$(env PATH)"}), "Cannot find bedrock_runtime_data")
     local link = assert(find_file("prelink.exe", {"$(env PATH)"}), "Cannot find prelink.exe")
@@ -13,13 +13,13 @@ before_link(function(target)
 
     os.execv(link,
         table.join({vformat("%s-%s-%s", config.get("target_type") or "server", config.get("plat"), config.get("arch")),
-                    vformat("$(buildir)/.prelink"), data}, target:objectfiles()))
-    target:add("linkdirs", "$(buildir)/.prelink/lib")
+                    vformat("$(builddir)/.prelink"), data}, target:objectfiles()))
+    target:add("linkdirs", "$(builddir)/.prelink/lib")
     target:add("shflags", "bedrock_runtime_api.lib", {
         force = true
     })
 end)
 after_link(function(target)
-    os.rm("$(buildir)/.prelink/lib/*.lib")
+    os.rm("$(builddir)/.prelink/lib/*.lib")
 end)
 rule_end()
